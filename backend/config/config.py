90f876a2279cc9de9b242bffe443088ed8cd4ccb
Aartikkk <aartikrk4@gmail.com>
2025-11-22 12:39:31 -0600

    Cleared backend

--- backend/config/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///../instance/mbs.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
